1474aa117d4ad664b9f13c3c0e190ff5baff546e
LuxSolace <71519212+LuxSolace@users.noreply.github.com>
2022-11-19 14:46:42 +0100

    Update function

--- script.js
+++ script.js
@@ -1,55 +1,25 @@
-"use strict";
+"use strict"
 
-const Player = (name, marker) => {
+const Player = (name, marker, isTheirTurn) => {
     let score = 0;
     let board = [
         0, 0, 0,
         0, 0, 0,
         0, 0, 0
     ];
-    return {name, marker, score, board}
-}
 
-const gameController = (() => {
-    let playerOne;
-    let playerTwo;
-
-    const createPlayer = (formData) => {
-        playerOne = Player(formData.get("firstPlayerName"), formData.get("marker"));
-        playerTwo = Player(formData.get("secondPlayerName"), playerOne.marker === "O" ? "X" : "O");
-    }
-
-    const startGame = (formData) => {
-        createPlayer(formData);
-        domElement.createBoard();
-    }
-
-    return {startGame, playRound, currentPlayer}
-})();
-
-const domElement = (() => {
-    const form = document.getElementById("form");
-    const boardContainer = document.getElementById("board-container");
-    
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const formData = new FormData(form);
-        gameController.startGame(formData);
-    })
-
-    const createBoard = () => {
-        let gameBoardHtml = ``;
-        gameBoard.board.forEach((item, index) => {
-            gameBoardHtml += `<div 
-            data-array-index="${index}"
-            onclick="gameController.playRound(this)"
-            >${item}</div>`
-        });
-        boardContainer.innerHTML = gameBoardHtml;
+    const changeTurn = () => {
+        isTheirTurn = !isTheirTurn;
     };
 
-    return {form, boardContainer, createBoard}
-})();
+    const getName = () => name;
+    const getMarker = () => marker;
+    const getScore = () => score;
+    const getBoard = () => board;
+    const getTurn = () => isTheirTurn;
+
+    return {getName, getMarker, getScore, getBoard, board, getTurn, changeTurn}
+};
 
 const gameBoard = (() => {
     let board = [
@@ -103,3 +73,77 @@ const gameBoard = (() => {
 
     return {board, winningCombo}
 })();
+
+const gameController = (() => {
+
+    let playerOne = {};
+    let playerTwo = {};
+    let currentPlayer;
+
+    const createPlayer = (formData) => {
+        playerOne = Player(formData.get("firstPlayerName"), formData.get("marker"), true);
+        playerTwo = Player(formData.get("secondPlayerName"), playerOne.marker === "O" ? "X" : "O", !playerOne.getTurn());
+    }
+
+    const startGame = (formData) => {
+        createPlayer(formData);
+        domElement.createBoard();
+    }
+
+    const getCurrentPlayer = () => {
+        if (playerOne.getTurn() === true) {
+            currentPlayer = playerOne
+        } else {
+            currentPlayer = playerTwo
+        }
+    };
+
+    const playRound = (divClicked) => {
+        getCurrentPlayer();
+    }
+
+    return {startGame, playRound}
+})();
+
+const domElement = (() => {
+    const form = document.getElementById("form");
+    const boardContainer = document.getElementById("board-container");
+    
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const formData = new FormData(form);
+        gameController.startGame(formData);
+    })
+
+    const createBoard = () => {
+        let gameBoardHtml = ``;
+        gameBoard.board.forEach((item, index) => {
+            gameBoardHtml += `<div 
+            data-array-index="${index}"
+            onclick="gameController.playRound(this)"
+            >${item}</div>`
+        });
+        boardContainer.innerHTML = gameBoardHtml;
+    };
+
+    return {form, boardContainer, createBoard}
+})();
+
+function compareNestedArray(nestedArr, arr) {
+
+    let result;
+
+    for (let i = 0; i < nestedArr.length; i++) {
+
+        result = nestedArr[i].every((element, index) => {
+            return element === arr[index]
+        })
+
+        if (result === true) {
+            break;
+        }
+
+    }
+
+    return result;
+}
